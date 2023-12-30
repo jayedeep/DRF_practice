5e952c2794547ca014d428403af51b13e6bc4e8a
jaydeep <jaydeeptalaviya8@gmail.com>
2023-12-30 16:48:10 +0530

    add field validator, object level validator and custom validator

--- Deserializer/base/serializers.py
+++ Deserializer/base/serializers.py
@@ -1,9 +1,33 @@
 from rest_framework import serializers
 from .models import Student
 
+
+def custom_validation(value):
+    print(">>>>>>",value)
+    if value[0] == 'z':
+        raise serializers.ValidationError("Please Name Should not start with Z")
+    return value
+
 class StudentSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=50)
+    name = serializers.CharField(max_length=50,validators=[custom_validation])
     email = serializers.EmailField()
+
+    def validate_name(self,value):
+        if value in Student.objects.all().values_list('name',flat=True):
+            raise serializers.ValidationError("Student with this name already exists")
+        return value
+
+    def validate_email(self, value):
+        if value in Student.objects.all().values_list('email', flat=True):
+            raise serializers.ValidationError("Student with this email already exists")
+        return value
+
+    def validate(self,data):
+        name = data.get('name',False)
+        email = data.get('email',False)
+        if name.lower() != name or email.lower() != email:
+            raise serializers.ValidationError("Student name or email should be in Lower case Always")
+        return data
 
     def create(self,validated_data):
         return Student.objects.create(**validated_data)
