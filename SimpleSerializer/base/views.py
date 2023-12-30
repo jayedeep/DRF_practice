56f029b72bf16cb5e2dbbcc6445e089005ec1ce3
jaydeep <jaydeeptalaviya8@gmail.com>
2023-12-30 11:42:40 +0530

    add changes to render serialized data using JsonResponse

--- SimpleSerializer/base/views.py
+++ SimpleSerializer/base/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render,get_object_or_404,HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,JsonResponse
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
+
 # Create your views here.
 
 
@@ -9,13 +11,15 @@ def get_student(request,id):
     student = get_object_or_404(Student,id=id)
     serializer = StudentSerializer(student)
     data = serializer.data
-    json_data = JSONRenderer().render(data)
-    return HttpResponse(json_data,content_type="application/json")
+    # json_data = JSONRenderer().render(data)
+    # return HttpResponse(json_data,content_type="application/json")
+    return JsonResponse(data,safe =False)
 
 
 def all_student(request):
     student = Student.objects.all()
     serializer = StudentSerializer(student,many=True)
     data = serializer.data
-    json_data = JSONRenderer().render(data)
-    return HttpResponse(json_data,content_type="application/json")
+    # json_data = JSONRenderer().render(data)
+    # return HttpResponse(json_data,content_type="application/json")
+    return JsonResponse(data,safe=False)
