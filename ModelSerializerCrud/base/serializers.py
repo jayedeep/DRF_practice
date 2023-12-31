45a58c01a34c4c0c687e6b01f2ea3202d920b59f
jaydeep <jaydeeptalaviya8@gmail.com>
2023-12-31 10:38:17 +0530

    added to_representation method in modelserializer

--- ModelSerializerCrud/base/serializers.py
+++ ModelSerializerCrud/base/serializers.py
@@ -6,3 +6,11 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ['id', 'name', 'email', 'bod']
+        extra_kwargs = {'email':{'read_only':True}}
+
+
+
+    def to_representation(self,instance):
+        customization = super().to_representation(instance)
+        customization['name_with_email'] = instance.name + ' ' + instance.email
+        return customization
