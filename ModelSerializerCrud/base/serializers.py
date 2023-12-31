25249dfe39b3f33e4ff8c942cadf34f1a65c360f
jaydeep <jaydeeptalaviya8@gmail.com>
2023-12-31 11:18:15 +0530

    added serializermethod to add extra field in serializer to be render

--- ModelSerializerCrud/base/serializers.py
+++ ModelSerializerCrud/base/serializers.py
@@ -4,11 +4,15 @@ from .models import Student
 
 class StudentSerializer(serializers.ModelSerializer):
     email_custom = serializers.CharField(source = 'email')
+    custom_name_email = serializers.SerializerMethodField()
     class Meta:
         model = Student
-        fields = ['id', 'name', 'email', 'bod','email_custom']
+        fields = '__all__'
         extra_kwargs = {'email':{'read_only':True}}
 
+    def get_custom_name_email(self, obj):
+        # This method computes the value for the 'custom_field' in the serialized output
+        return f"Custom value for {obj.email.upper()}"
 
     def to_representation(self,instance):
         customization = super().to_representation(instance)
