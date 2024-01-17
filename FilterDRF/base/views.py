a654da59fb1e273e225f4915d78f8ab0f44669ff
jaydeep <jaydeeptalaviya8@gmail.com>
2024-01-17 21:18:46 +0530

    added order filter with rest framework

--- FilterDRF/base/views.py
+++ FilterDRF/base/views.py
@@ -7,8 +7,12 @@ from rest_framework.authentication import SessionAuthentication
 from rest_framework.throttling import AnonRateThrottle,UserRateThrottle
 # Create your views here.
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+
 class StudentCRUD(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['name','email']
+    # filter_backends = [DjangoFilterBackend]
+    # filterset_fields = ['name','email']
+    filter_backends =[OrderingFilter]
+    ordering_fields = ['name']
